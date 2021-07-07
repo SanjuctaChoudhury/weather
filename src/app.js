c96e93ecfe5d66f2f8d182ad855b7aa400672216
wander789 <86791627+wander789@users.noreply.github.com>
2021-07-07 21:54:20 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express=require("express")
 const app= express()
 const path=require("path")
-const port =3000
+const port =process.env.PORT||3000
 //public folder's static path
 const staticp=path.join(__dirname,"../public")
 console.log(staticp)
